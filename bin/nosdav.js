46236ad0879b515d5314703d383e3143fba3f09e
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-03 07:48:06 +0200

    refactor cors headers closes #1

--- bin/nosdav.js
+++ bin/nosdav.js
@@ -1,90 +1,87 @@
 #!/usr/bin/env node
 
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
 
+const { verifySignature } = require('nostr-tools')
 
-const { verifySignature } = require('nostr-tools');
-
-
-const port = process.argv[4] ? parseInt(process.argv[4]) : 3118;
-const rootDir = 'data';
+const port = process.argv[4] ? parseInt(process.argv[4]) : 3118
+const rootDir = 'data'
 
 const options = {
   key: fs.readFileSync(process.argv[2]),
   cert: fs.readFileSync(process.argv[3])
-};
+}
 
+const isValidNostr = nostr => {
+  return /^[0-9a-f]{64}$/.test(nostr)
+}
 
-const isValidNostr = (nostr) => {
-  return /^[0-9a-f]{64}$/.test(nostr);
-};
-
-const isValidAuthorizationHeader = (authorization) => {
-
+const isValidAuthorizationHeader = authorization => {
   console.log('authorization', authorization)
   const base64String = authorization.replace('Nostr ', '')
 
   // Decode the base64-encoded string and parse the JSON object
-  const decodedString = Buffer.from(base64String, "base64").toString("utf-8");
-  const event = JSON.parse(decodedString);
+  const decodedString = Buffer.from(base64String, 'base64').toString('utf-8')
+  const event = JSON.parse(decodedString)
 
   // Print the object
-  console.log(event);
+  console.log(event)
 
-  const isVerified = verifySignature(event);
+  const isVerified = verifySignature(event)
   if (isVerified) {
     return event.pubkey
   }
-
 }
 
 const isValidTargetDir = (targetDir, nostr) => {
-  const targetSegments = targetDir.split('/').filter(segment => segment !== '');
-  return targetSegments.length === 1 && targetSegments[0] === nostr;
-};
+  const targetSegments = targetDir.split('/').filter(segment => segment !== '')
+  return targetSegments.length === 1 && targetSegments[0] === nostr
+}
 
-const getContentType = (ext) => {
+const getContentType = ext => {
   switch (ext) {
     case '.txt':
-      return 'text/plain';
+      return 'text/plain'
     case '.html':
-      return 'text/html';
+      return 'text/html'
     case '.json':
-      return 'application/json';
+      return 'application/json'
     default:
-      return 'application/octet-stream';
+      return 'application/octet-stream'
   }
-};
+}
+
+function setCorsHeaders(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+}
 
 const server = https.createServer(options, (req, res) => {
-  const { method, url: reqUrl, headers } = req;
-  const { pathname } = url.parse(reqUrl);
-  const targetDir = path.dirname(pathname);
+  const { method, url: reqUrl, headers } = req
+  const { pathname } = url.parse(reqUrl)
+  const targetDir = path.dirname(pathname)
 
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
+  setCorsHeaders(res)
 
   // Set CORS options
   const corsOptions = {
     origin: 'https://example.com',
     methods: ['GET', 'PUT'],
     allowedHeaders: ['Content-Type']
-  };
+  }
 
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
-    res.writeHead(204, corsOptions);
-    res.end();
-    return;
+    res.writeHead(204, corsOptions)
+    res.end()
+    return
   }
-
 
   if (method === 'PUT') {
     const nostr = headers.authorization.replace('Nostr ', '')
@@ -95,76 +92,80 @@ const server = https.createServer(options, (req, res) => {
 
     var pubkey = isValidAuthorizationHeader(headers.authorization)
     if (!nostr || !pubkey) {
+      res.statusCode = 401
+      res.end(
+        'Unauthorized: "nostr" header must be a 32 character lowercase hex string'
+      )
+      console.log(
+        'Unauthorized: "nostr" header must be a 32 character lowercase hex string'
+      )
 
-      res.statusCode = 401;
-      res.end('Unauthorized: "nostr" header must be a 32 character lowercase hex string');
-      console.log('Unauthorized: "nostr" header must be a 32 character lowercase hex string');
-
-      return;
+      return
     }
 
     // Check if the target directory is valid
     if (!isValidTargetDir(targetDir, pubkey)) {
-      res.statusCode = 403;
-      res.end('Forbidden: Target directory structure is invalid');
-      console.log('Forbidden: Target directory structure is invalid', targetDir, nostr);
-      return;
+      res.statusCode = 403
+      res.end('Forbidden: Target directory structure is invalid')
+      console.log(
+        'Forbidden: Target directory structure is invalid',
+        targetDir,
+        nostr
+      )
+      return
     }
 
-    const targetPath = path.join('.', rootDir, pathname);
+    const targetPath = path.join('.', rootDir, pathname)
 
     // Ensure target directory exists
-    fs.mkdir(path.dirname(targetPath), { recursive: true }, (err) => {
+    fs.mkdir(path.dirname(targetPath), { recursive: true }, err => {
       if (err) {
-        console.error(err);
-        res.statusCode = 500;
-        res.end('Error creating directory');
-        console.log('Error creating directory');
-        return;
+        console.error(err)
+        res.statusCode = 500
+        res.end('Error creating directory')
+        console.log('Error creating directory')
+        return
       }
 
       // Save the file
-      const writeStream = fs.createWriteStream(targetPath);
-      req.pipe(writeStream);
+      const writeStream = fs.createWriteStream(targetPath)
+      req.pipe(writeStream)
       writeStream.on('finish', () => {
-        res.statusCode = 201;
-        res.end('File created');
-        console.log('File created');
-      });
-      writeStream.on('error', (err) => {
-        console.error(err);
-        res.statusCode = 500;
-        res.end('Error writing file');
-        console.log('Error writing file');
-      });
-    });
+        res.statusCode = 201
+        res.end('File created')
+        console.log('File created')
+      })
+      writeStream.on('error', err => {
+        console.error(err)
+        res.statusCode = 500
+        res.end('Error writing file')
+        console.log('Error writing file')
+      })
+    })
   } else if (method === 'GET') {
-    const targetPath = path.join('.', rootDir, pathname);
-
-
+    const targetPath = path.join('.', rootDir, pathname)
 
     // Read the file
     fs.readFile(targetPath, (err, data) => {
       if (err) {
-        console.error(err);
-        res.statusCode = 404;
-        res.end('File not found');
-        console.log('File not found');
+        console.error(err)
+        res.statusCode = 404
+        res.end('File not found')
+        console.log('File not found')
       } else {
-        const contentType = getContentType(path.extname(targetPath));
-        res.setHeader('Content-Type', contentType);
-        res.statusCode = 200;
-        res.end(data);
+        const contentType = getContentType(path.extname(targetPath))
+        res.setHeader('Content-Type', contentType)
+        res.statusCode = 200
+        res.end(data)
       }
-    });
+    })
   } else {
-    res.statusCode = 405;
-    res.end('Method not allowed');
-    console.log('Method not allowed');
+    res.statusCode = 405
+    res.end('Method not allowed')
+    console.log('Method not allowed')
   }
-});
+})
 
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
+  console.log(`Server running at http://localhost:${port}`)
+})
