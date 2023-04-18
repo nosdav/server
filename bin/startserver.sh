42f11f1aef4bbf926079d3659a3cd04f245273d2
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-18 18:45:20 +0200

    include subj in cert creation

--- bin/startserver.sh
+++ bin/startserver.sh
@@ -10,8 +10,8 @@ fi
 
 if [ ! -f "$PRIVKEY_PATH" ] || [ ! -f "$FULLCHAIN_PATH" ]; then
   echo "Generating self-signed certificates..."
-  openssl req -outform PEM -keyform PEM -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout ./privkey.pem -days 365 -out ./fullchain.pem
+  openssl req -outform PEM -keyform PEM -new -x509 -sha256 -newkey rsa:2048 -nodes -subj "/C=US/ST=California/L=San Francisco/O=My Company/CN=localhost" -keyout ./privkey.pem -days 365 -out ./fullchain.pem
 fi
 
 echo "Starting server..."
-bin/nosdav.js
+./bin/nosdav.js
