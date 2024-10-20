113914e9596a94f6394641115ef9b071aae2106b
Melvin Carvalho <melvincarvalho@gmail.com>
2024-10-20 17:02:17 +0200

    remove path check

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ import path from 'path'
  * @param {Array<string>} owners - The public keys of the owners (used in 'singleuser' mode).
  * @returns {function} A request handler function that handles incoming HTTP requests based on the specified rootDir, mode, and owners.
  */
-function createRequestHandler(rootDir, mode, owners) {
-  return function handleRequest(req, res) {
+function createRequestHandler (rootDir, mode, owners) {
+  return function handleRequest (req, res) {
     const { method, url: reqUrl, headers } = req
     const { pathname } = url.parse(reqUrl)
     const adjustedPathname = pathname.endsWith('/') ? `${pathname}index.html` : pathname
@@ -147,7 +147,7 @@ const isValidTargetDir = (targetDir, nostr, mode) => {
  *
  * @param {http.ServerResponse} res - The response object.
  */
-function setCorsHeaders(res) {
+function setCorsHeaders (res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
@@ -161,7 +161,7 @@ function setCorsHeaders(res) {
  * @param {string} authorization - The authorization header value.
  * @returns {(string|null)} The public key if the header is valid, null otherwise.
  */
-function isValidAuthorizationHeader(authorization) {
+function isValidAuthorizationHeader (authorization) {
   console.log('authorization', authorization)
   const base64String = authorization.replace('Nostr ', '')
 
@@ -184,7 +184,7 @@ function isValidAuthorizationHeader(authorization) {
  * @param {http.IncomingMessage} req - The request object.
  * @param {http.ServerResponse} res - The response object.
  */
-function handleOptions(req, res) {
+function handleOptions (req, res) {
   // Set CORS options
   const corsOptions = {
     origin: 'https://example.com',
@@ -208,7 +208,7 @@ function handleOptions(req, res) {
  * @param {string} mode - The server mode ('singleuser' or 'multiuser').
  * @param {Array<string>} owners - The public keys of the owners (used in 'singleuser' mode).
  */
-function handlePut(
+function handlePut (
   req,
   res,
   headers,
@@ -271,22 +271,22 @@ function handlePut(
   // Check if the target path is within the root directory
   const resolvedRootDir = path.resolve(rootDir)
 
-  if (mode === 'singleuser') {
-    if (!targetPath.startsWith(resolvedRootDir)) {
-      res.statusCode = 403
-      res.end('Forbidden: Target path is outside the root directory')
-      console.log('Forbidden: Target path is outside the root directory', targetPath, rootDir, mode)
-      return
-    }
-  } else if (mode === 'multiuser') {
-    const resolvedPubKeyDir = path.resolve(rootDir, pubkey)
-    if (!targetPath.startsWith(resolvedPubKeyDir)) {
-      res.statusCode = 403
-      res.end('Forbidden: Target path is outside the user directory')
-      console.log('Forbidden: Target path is outside the user directory', targetPath, resolvedPubKeyDir, mode)
-      return
-    }
-  }
+  // if (mode === 'singleuser') {
+  //   if (!targetPath.startsWith(resolvedRootDir)) {
+  //     res.statusCode = 403
+  //     res.end('Forbidden: Target path is outside the root directory')
+  //     console.log('Forbidden: Target path is outside the root directory', targetPath, rootDir, mode)
+  //     return
+  //   }
+  // } else if (mode === 'multiuser') {
+  //   const resolvedPubKeyDir = path.resolve(rootDir, pubkey)
+  //   if (!targetPath.startsWith(resolvedPubKeyDir)) {
+  //     res.statusCode = 403
+  //     res.end('Forbidden: Target path is outside the user directory')
+  //     console.log('Forbidden: Target path is outside the user directory', targetPath, resolvedPubKeyDir, mode)
+  //     return
+  //   }
+  // }
 
   // Ensure target directory exists
   fs.mkdir(path.dirname(targetPath), { recursive: true }, err => {
@@ -323,7 +323,7 @@ function handlePut(
  * @param {string} pathname - The requested file's path.
  * @param {string} rootDir - The root directory for all files.
  */
-function handleGet(req, res, rootDir, pathname) {
+function handleGet (req, res, rootDir, pathname) {
   const targetPath = rootDir.startsWith('/')
     ? path.join(rootDir, pathname)
     : path.join('.', rootDir, pathname)
